6b775ecc5a6bab3391d9791e547354c822524bc6
allanx <allan.jackson.programmer@gmail.com>
2026-04-29 04:27:13 -0300

    refactor: modularizar a tela de titulo e criar funcoes auxiliares

--- EsqueletoRPG.java
+++ EsqueletoRPG.java
@@ -8,29 +8,36 @@ import java.util.Scanner;
 public class EsqueletoRPG {
 	
 	public static final int ALTURA_TELA = 30;
-	
-	
-	public static void main(String[] args) {
-		exibirMenuInicial();
+	public static void exibirLogo() {
+		System.out.println();
+		System.out.println("                        BY                        ");
+		System.out.println("         ALLAN JACKSON SILVA OLIVEIRA             ");
+		System.out.println("      ____  ______   _________  ______   ___      ");
+		System.out.println("     |    |/      \\ /         \\/      \\ /   \\ ");
+		System.out.println("     |    |   __   |    ______|   __   |     |    ");
+		System.out.println("     |    |  |  |  |   |  ____|  |  |  |     |    ");
+		System.out.println("     |    |  |  |  |   | /_   \\  |  |  |\\___/   ");
+		System.out.println("   __|    |  \\__/  |   |___|  |  |__|  | ___     ");
+		System.out.println("  |       |        |          |        |/   \\    ");
+		System.out.println("   \\_____/ \\______/ \\_________/\\______/ \\___/");
 	}
 	
+	public static void main(String[] args) {
+		exibirTelaTitulo();
+	}
+
 	/**
-	 * Exibe o MENU INICIAL para o usuário.
-	 * Aqui o usuário tem a opção de começar o jogo,
-	 * ler as regras ou sair do terminal
-	 * Se a opção for inválida o menu é exibido outra vez
+	 * Exibe a tela de título do jogo
+	 * com o logo e o menu inicial.
 	 */
-	public static void exibirMenuInicial() {
-		
-		 
-		System.out.println("NOME DO JOGO");
-		System.out.println("------------");
-		System.out.println("1 - Iniciar jogo");
-		System.out.println("2 - Ler regras");
-		System.out.println("3 - Sair");
-		
+	public static void exibirTelaTitulo() {
+		exibirLogo();
+		exibirMenuTitulo();
+
 		Scanner entrada = new Scanner(System.in);
 		System.out.print("Escolha: ");
+
+		//TODO: criar método para realizar a leitura de dados do usuário com a devida validação e tratamento de erros
 		int op = entrada.nextInt();
 
 		limparTela();
@@ -41,20 +48,28 @@ public class EsqueletoRPG {
 				iniciarJogo();
 				break;
 			case 2:
-				/*TODO: MOSTRAR AS REGRAS*/
-				//código provisório com msg e volta para o menu inicial
-				System.out.println("\n\nEis aí as regras:\nHAHAHAHAHAHAHA!\n\n");
-				exibirMenuInicial();
+				exibirRegras();
+				exibirTelaTitulo();
 				break;
 			case 3:
 				System.out.println("\n\nAté mais!\n\n");
 				System.exit(0);
 				break;
 			default:
-				exibirMenuInicial();
+				exibirTelaTitulo();
 		}
 	}
-	
+
+	/**
+	 * Exibe o MENU INICIAL com as opções para o usuário.
+	 */
+	public static void exibirMenuTitulo() {
+		System.out.println("\n-----------------------------------------------\n");
+		System.out.println("[1] -> Iniciar jogo");
+		System.out.println("[2] -> Ler regras");
+		System.out.println("[3] -> Sair");
+	}
+
 	/**
 	 * Limpa a tela do terminal.
 	 * Limpa a tela imprimindo várias quebras de linhas vazias
@@ -68,6 +83,22 @@ public class EsqueletoRPG {
 
 
 	public static void iniciarJogo() {
+		introducao();
+		exibirDesafio1();
+	}
+
+	/**
+	 * Exibe a história introdutório do jogo
+	 */
+	public static void introducao() {
+		//TODO: desenvolver a história de introdução do jogo
+		System.out.println("Tudo começa com... o roteirista escrevendo a PORCARIA da história.\nFim da introdução!");
+	}
+
+	/**
+	 * Exibe para o usuário o primeiro desafio do jogo
+	 */
+	public static void exibirDesafio1() {
 		String pergunta = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus interdum, tortor sed bibendum bibendum, arcu orci?";
 		String op1 = "Lorem ipsum 1";
 		String op2 = "Lorem ipsum 2";
@@ -90,7 +121,7 @@ public class EsqueletoRPG {
 		boolean respostaValida = false;
 
 		do { //repete enquanto a resposta não for válida
-			System.out.print("Diga sua resposta: ");
+			System.out.print("Diga sua resposta (ou 0 para regras): ");
 			op = entrada.nextInt();
 
 			respostaValida = validarResposta(op);
@@ -109,6 +140,27 @@ public class EsqueletoRPG {
 		System.out.println("Você completou a primeira missão com sucesso!");
 		System.out.println("Mas ainda não acabou, existem novos desafios pela frente!");
 		System.out.println("Continua...");
+	}
+
+
+	/**
+	 * Exibe as regras do jogo para o usuário
+	 * IDEA: as regras podem ser verificadas durante o próprio jogo
+	 * e deve voltar para a mesma tela que estava
+	 */
+	private static void exibirRegras() {
+		Scanner pause = new Scanner(System.in);
+
+		//código provisório com msg e volta para o menu inicial
+		System.out.println("\n\nEis aí as regras:\nHAHAHAHAHAHAHA!\n\n");
+		System.out.println("1. Eis aí as regras:\nHAHAHAHAHAHAHA!");
+		System.out.println("2. Eis aí as regras:\nHAHAHAHAHAHAHA!");
+		System.out.println("3. Eis aí as regras:\nHAHAHAHAHAHAHA!");
+		System.out.println("4. Eis aí as regras:\nHAHAHAHAHAHAHA!");
+		System.out.println("\n\nEis aí as regras:\nHAHAHAHAHAHAHA!\n\n");
+		System.out.printf("Tecle ENTER para continuar...");
+
+		pause.nextLine();
 	}
 
 	/**
